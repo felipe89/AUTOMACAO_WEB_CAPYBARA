18ef7eea22ff4198acdde5e0959928d21998802c
felipe89 <gfelipe.trindade@gmail.com>
2020-10-31 19:16:07 -0300

    acessando uma pagina web

---
+++ features/support/env.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require 'yaml'
+require 'pry'
+ require 'capybara/cucumber'
+ require 'capybara/poltergeist'
+ require 'selenium-webdriver'
+
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.app_host = 'https://automacaocombatista.herokuapp.com'
+  config.default_max_wait_time = 5
+end
+ 

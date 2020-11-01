413a20472c185f8bd53259881f1dcd4107b35b35
felipe89 <gfelipe.trindade@gmail.com>
2020-11-01 14:01:21 -0300

    validaçao de elementos visiveis

---
+++ features/step_definitions/verificando_elementos_visiveis.rb
@@ -0,0 +1,15 @@
+Quando('clico no botão') do
+    visit 'buscaelementos/botoes'
+    find('#teste').click
+end
+  
+  Então('verifico se o texto apareceu na tela com sucesso') do
+#Validar mensagem na tela tipo texto
+    @texto = find('#div1')
+    expect(@texto.text).to eql 'Você Clicou no Botão!'
+
+    page.assert_text(text, 'Você Clicou no Botão!')
+    page.has_text?('Você Clicou no Botão!')
+    have_text('Você Clicou no Botão!')
+
+end
